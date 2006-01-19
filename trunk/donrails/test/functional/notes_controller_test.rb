5cb0b29028c03cd4574f14d6a31fa308444e3ee4
araki <araki@1d00b2ed-99fd-0310-9764-f56fb2dcbaab>
2006-01-19 16:22:39 +0000

    add many test methods

--- trunk/donrails/test/functional/notes_controller_test.rb
+++ trunk/donrails/test/functional/notes_controller_test.rb
@@ -12,34 +12,162 @@ class NotesControllerTest < Test::Unit::TestCase
   end
 
 
-  def test_trackback
-    post :trackback,
-    :id => 1,
-    :title => 'title test util',
-    :excerpt => 'excerpt text',
-    :url => "http://test.example.com/blog/",
-    :blog_name => 'test of donrails'
+#   def test_trackback
+#     post :trackback,
+#     :id => 1,
+#     :title => 'title test util',
+#     :excerpt => 'excerpt text',
+#     :url => "http://test.example.com/blog/",
+#     :blog_name => 'test of donrails'
 
-    require_response_body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<response>\n  <error>0</error>\n  <message>success</message>\n</response>\n"
+#     require_response_body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<response>\n  <error>0</error>\n  <message>success</message>\n</response>\n"
+#     assert_response :success
+#     # puts @response.body
+#     assert_equal require_response_body, @response.body
+#   end
+
+  def test_index
+    get :index
     assert_response :success
-    # puts @response.body
-    assert_equal require_response_body, @response.body
   end
+
+  def test_search
+    get :search, :q => 'sendmail'
+    assert_response :success
+  end
+
+  def test_show_search_noteslist
+    get :search, :q => 'sendmail'
+    assert_response :success
+  end
+  
 
   def test_pick_article_a
     get :pick_article_a, :pickid => 1
-#    puts @response.body
     assert_response :success
   end
+  def test_pick_article_a2
+    get :pick_article_a2, :pickid => 1
+    assert_response :success
+  end
+  def test_comment_form_a
+    get :comment_form_a, :id => 1
+    assert_response :success
+  end
+  def test_articles_long
+    get :articles_long
+    assert_response :success
+  end
+  def test_indexabc
+    get :indexabc, :nums => '200401a'
+    assert_response 302
+    assert_equal('http://test.host/notes/tendays?day=01&month=01&year=2004', @response.headers['location'])
+    get :indexabc, :nums => '200401b'
+    assert_response 302
+    assert_equal('http://test.host/notes/tendays?day=11&month=01&year=2004', @response.headers['location'])
+    get :indexabc, :nums => '200401c'
+    assert_response 302
+    assert_equal('http://test.host/notes/tendays?day=21&month=01&year=2004', @response.headers['location'])
+
+    get :indexabc, :nums => '20040105c'
+    assert_response 404
+  end
+
+  def test_dateparse
+  end
+
+  def test_noteslist
+    get :noteslist
+    assert_response 200
+  end
+
+  def test_parse_nums
+    get :parse_nums
+    assert_response 302
+
+    get :parse_nums, :nums => '200403051'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/d?notice=200403051', @response.headers['location'])
+
+    get :parse_nums, :nums => '20040305'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/2004/03/05', @response.headers['location'])
+
+    get :parse_nums, :nums => '2004-03-05'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/2004/03/05', @response.headers['location'])
+
+    get :parse_nums, :nums => '2004-3-05'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/2004/3/05', @response.headers['location'])
+
+    get :parse_nums, :nums => '20040305.html'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/2004/03/05', @response.headers['location'])
+    get :parse_nums, :nums => '2004-03-05.html'
+    assert_response 302
+#    puts @response.body
+    assert_equal('http://test.host/notes/2004/03/05', @response.headers['location'])
+  end
+
+  def test_rdf_recent
+    get :rdf_recent
+    assert_response :success
+  end
+
+  def test_rdf_article
+    get :rdf_article, :id => 1
+    assert_response :success
+  end
+
+  def test_rdf_search
+    get :rdf_search, :q => 'sendmail'
+    assert_response :success
+  end
+
+  def test_rdf_category
+    get :rdf_category, :category => 'no category'
+    assert_response 302
+
+    get :rdf_category, :category => 'misc'
+    assert_response :success
+  end
+
+  def test_recent_trigger_title_a
+    get :recent_trigger_title_a, :trigger => 'recents'
+    assert_response :success
+    get :recent_trigger_title_a, :trigger => 'trackbacks'
+    assert_response :success
+    get :recent_trigger_title_a, :trigger => 'comments'
+    assert_response :success
+    get :recent_trigger_title_a, :trigger => 'long'
+    assert_response :success
+  end
+
+  def test_recent_category_title_a
+    get :recent_category_title_a, :category => 'misc'
+    assert_response :success
+
+    get :recent_category_title_a, :category => 'null'
+    assert_response :success
+  end
+
+  def test_category_select_a
+    get :category_select_a
+    assert_response :success
+  end
+
+
 
   def test_catch_ping
     post :catch_ping, :category => 'misc', :blog_name => 'test blog',
     :title => 'test title', :excerpt => 'test excerpt', :url => 'http://localhost/test/blog'
     assert_response :success
-
-    get :catch_ping, :category => 'misc', :blog_name => 'test blog',
-    :title => 'test title', :excerpt => 'test excerpt', :url => 'http://localhost/test/blog'
-    assert_response 404
   end
 
   def test_recent_category_title_a
